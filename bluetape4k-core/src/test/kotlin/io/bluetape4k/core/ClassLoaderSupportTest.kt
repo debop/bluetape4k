3efa079dccdf9065988c416f63bae6f347bd6926
Debop <debop@hpcnt.com>
2023-04-10 17:21:33 +0900

    ClassLoader support 함수 추가

---
+++ bluetape4k-core/src/test/kotlin/io/bluetape4k/core/ClassLoaderSupportTest.kt
@@ -0,0 +1,24 @@
+package io.bluetape4k.core
+
+import io.bluetape4k.logging.KLogging
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class ClassLoaderSupportTest {
+
+    companion object : KLogging()
+
+    @Test
+    fun `load ClassLoader by current context`() {
+        val currentClassLoader = getContextClassLoader()
+        val systemClassLoader = getSystemClassLoader()
+
+        currentClassLoader shouldBeEqualTo systemClassLoader
+    }
+
+    @Test
+    fun `get class loader from class type`() {
+        getClassLoader(KLogging::class) shouldBeEqualTo getContextClassLoader()
+        getClassLoader(ByteArrays::class) shouldBeEqualTo getClassLoader<ByteArrays>()
+    }
+}
