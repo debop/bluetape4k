d77411899a8dcb29b0f8f555aae9321b659c5108
Debop <debop@hpcnt.com>
2023-04-16 20:19:50 +0900

    ClosedRange 테스트 코드 추가

---
+++ bluetape4k-core/src/test/kotlin/io/bluetape4k/ranges/ClosedRangeSupporTest.kt
@@ -0,0 +1,46 @@
+package io.bluetape4k.ranges
+
+import io.bluetape4k.logging.KLogging
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeTrue
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ClosedRangeSupporTest {
+
+    companion object: KLogging()
+
+    private fun longRanges(vararg ranges: IntRange): List<LongRange> =
+        ranges.map { it.start.toLong()..it.endInclusive.toLong() }
+
+
+    @Test
+    fun `range is ascending`() {
+        longRanges(0..10, 1..5, 12..13).isAscending().shouldBeTrue()
+        longRanges(2..10, 1..5, 5..13).isAscending().shouldBeFalse()
+    }
+
+    @Test
+    fun `create closed range by BigDecimal`() {
+        val range = 5.toBigDecimal()..10.toBigDecimal()
+
+        Assertions.assertTrue { 6.toBigDecimal() in range }
+        Assertions.assertFalse { 3.toBigDecimal() in range }
+
+        range.contains(7.toBigDecimal()..10.toBigDecimal()).shouldBeTrue()
+        range.contains(7.toBigDecimal()..11.toBigDecimal()).shouldBeFalse()
+        range.contains(4.toBigDecimal()..10.toBigDecimal()).shouldBeFalse()
+    }
+
+    @Test
+    fun `create closed range by BigInteger`() {
+        val range = 5.toBigInteger()..10.toBigInteger()
+
+        Assertions.assertTrue { 6.toBigInteger() in range }
+        Assertions.assertFalse { 3.toBigInteger() in range }
+
+        range.contains(7.toBigInteger()..10.toBigInteger()).shouldBeTrue()
+        range.contains(7.toBigInteger()..11.toBigInteger()).shouldBeFalse()
+        range.contains(4.toBigInteger()..10.toBigInteger()).shouldBeFalse()
+    }
+}
