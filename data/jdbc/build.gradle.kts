b7d794e21cc92bb11b8170a122d71de3a119d4f7
Debop <debop@hpcnt.com>
2023-04-19 12:31:34 +0900

    add jdbc support methods

---
+++ data/jdbc/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("plugin.spring")
+}
+
+// NOTE: compileOnly 나 runtimeOnly로 지정된 Dependency를 testImplementation 으로도 지정하도록 합니다.
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    api(project(":bluetape4k-core"))
+    testImplementation(project(":bluetape4k-test-junit5"))
+    testImplementation(project(":bluetape4k-test-testcontainers"))
+
+    compileOnly(Libs.hikaricp)
+    compileOnly(Libs.tomcat_jdbc)
+
+    compileOnly(Libs.springBootStarter("jdbc"))
+    testImplementation(Libs.springBootStarter("test")) {
+        exclude(group = "junit", module = "junit")
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
+    runtimeOnly(Libs.h2)
+    testImplementation(Libs.testcontainers_mysql)
+    testImplementation(Libs.mysql_connector_java)
+    testImplementation(Libs.mariadb_java_client)
+}
