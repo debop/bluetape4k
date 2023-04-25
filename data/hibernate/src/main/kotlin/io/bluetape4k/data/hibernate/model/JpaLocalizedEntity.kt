287af741559089056d882db34969a0b56debf409
Debop <debop@hpcnt.com>
2023-04-25 14:05:37 +0900

    add hibernate localized entity example

---
+++ data/hibernate/src/main/kotlin/io/bluetape4k/data/hibernate/model/JpaLocalizedEntity.kt
@@ -0,0 +1,38 @@
+package io.bluetape4k.data.hibernate.model
+
+import io.bluetape4k.core.ValueObject
+import java.util.*
+
+interface JpaLocalizedEntity<LV : JpaLocalizedEntity.LocalizedValue> : PersistenceObject {
+
+    interface LocalizedValue : ValueObject
+
+    val localeMap: MutableMap<Locale, LV>
+
+    fun createDefaultLocalizedValue(): LV
+
+    /**
+     * 특정 지역에 해당하는 정보
+     *
+     * @param locale Locale 정보
+     * @return 특정 지역에 해당하는 정보
+     */
+    fun getLocalizedValue(locale: Locale): LV {
+        return getLocalizedValueOrDefault(locale)
+    }
+
+    /**
+     * 특정 지역의 정보를 가져옵니다. 만약 해당 지역의 정보가 없다면 엔티티의 정보를 이용한 정보를 제공합니다.
+     * @param locale 지역 정보
+     * @return 지역화 정보
+     */
+    fun getLocalizedValueOrDefault(locale: Locale = Locale.getDefault()): LV {
+        return localeMap[locale] ?: createDefaultLocalizedValue()
+    }
+
+    /**
+     * 현 Thread Context 에 해당하는 지역의 정보를 제공합니다.
+     * @return 지역화 정보
+     */
+    fun getCurrentLocalizedValue(): LV = getLocalizedValueOrDefault(Locale.getDefault())
+}
---
+++ data/hibernate/src/test/kotlin/io/bluetape4k/data/hibernate/mapping/localized/TravelContent.kt
@@ -0,0 +1,51 @@
+package io.bluetape4k.data.hibernate.mapping.localized
+
+import io.bluetape4k.core.ToStringBuilder
+import io.bluetape4k.data.hibernate.model.IntJpaEntity
+import io.bluetape4k.data.hibernate.model.JpaLocalizedEntity
+import org.hibernate.annotations.Cascade
+import org.hibernate.annotations.LazyCollection
+import org.hibernate.annotations.LazyCollectionOption
+import java.util.*
+import javax.persistence.*
+
+@Entity(name = "travel_content")
+@Access(AccessType.FIELD)
+class TravelContent(
+    var title: String,
+    var contents: String,
+    var creator: String,
+) : IntJpaEntity(), JpaLocalizedEntity<LocalTravelContent> {
+
+    @CollectionTable(name = "travel_content_locale_map", joinColumns = [JoinColumn(name = "travel_content_id")])
+    @MapKeyClass(Locale::class)
+    @MapKeyColumn(name = "locale_key", length = 256, nullable = false, unique = true)
+    @ElementCollection(targetClass = LocalTravelContent::class)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    override val localeMap: MutableMap<Locale, LocalTravelContent> = hashMapOf()
+
+    override fun createDefaultLocalizedValue(): LocalTravelContent {
+        return LocalTravelContent(this.title, this.contents)
+    }
+
+    override fun equalProperties(other: Any): Boolean {
+        return other is TravelContent &&
+            title == other.title &&
+            contents == other.contents &&
+            creator == other.creator
+    }
+
+    override fun buildStringHelper(): ToStringBuilder {
+        return super.buildStringHelper()
+            .add("title", title)
+            .add("contents", contents)
+            .add("creator", creator)
+    }
+}
+
+@Embeddable
+data class LocalTravelContent(
+    var title: String,
+    var content: String
+) : JpaLocalizedEntity.LocalizedValue
