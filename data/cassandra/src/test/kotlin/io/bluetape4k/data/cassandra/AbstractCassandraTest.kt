4bdbbfac0b60ca77ca38adf3bdcf999efbec8781
Debop <debop@hpcnt.com>
2023-04-27 23:50:15 +0900

    add cassandra support methods

--- data/cassandra/src/test/kotlin/io/bluetape4k/data/cassandra/AbstractCassandraTest.kt
+++ data/cassandra/src/test/kotlin/io/bluetape4k/data/cassandra/AbstractCassandraTest.kt
@@ -4,11 +4,14 @@ import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.CqlSessionBuilder
 import io.bluetape4k.logging.KLogging
 import io.bluetape4k.testcontainers.storage.Cassandra4Server
+import net.datafaker.Faker
 
 abstract class AbstractCassandraTest {
 
     companion object: KLogging() {
         const val DEFAULT_KEYSPACE = "examples"
+
+        val faker = Faker()
 
         val cassandra4 by lazy {
             Cassandra4Server.Launcher.cassandra4
---
+++ data/cassandra/src/test/kotlin/io/bluetape4k/data/cassandra/cql/AsyncCqlSessionSupportTest.kt
@@ -0,0 +1,64 @@
+package io.bluetape4k.data.cassandra.cql
+
+import io.bluetape4k.data.cassandra.AbstractCassandraTest
+import io.bluetape4k.junit5.coroutines.runSuspendWithIO
+import io.bluetape4k.logging.KLogging
+import org.amshove.kluent.shouldBeNull
+import org.amshove.kluent.shouldBeTrue
+import org.amshove.kluent.shouldNotBeNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicBoolean
+
+class AsyncCqlSessionSupportTest: AbstractCassandraTest() {
+
+    companion object: KLogging() {
+        private val initialized = AtomicBoolean()
+    }
+
+    @BeforeEach
+    fun setup() {
+        runSuspendWithIO {
+            if (initialized.compareAndSet(false, true)) {
+                session.executeSuspending("DROP TABLE IF EXISTS user")
+                session.executeSuspending("CREATE TABLE IF NOT EXISTS user (id text PRIMARY KEY, username text);")
+            }
+
+            session.executeSuspending("TRUNCATE user")
+            session.executeSuspending("INSERT INTO user (id, username) VALUES ('WHITE', 'Walter')")
+        }
+    }
+
+    @Test
+    fun `execute by cql in coroutines`() = runSuspendWithIO {
+        session.executeSuspending("SELECT * FROM user").one().shouldNotBeNull()
+
+        session.executeSuspending("DELETE FROM user WHERE id = 'WHITE'").wasApplied().shouldBeTrue()
+        session.executeSuspending("SELECT * FROM user").one().shouldBeNull()
+    }
+
+    @Test
+    fun `execute by cql with params in coroutines`() = runSuspendWithIO {
+        session.executeSuspending("SELECT * FROM user").one().shouldNotBeNull()
+
+        session.executeSuspending("DELETE FROM user WHERE id = ?", "WHITE").wasApplied().shouldBeTrue()
+        session.executeSuspending("SELECT * FROM user").one().shouldBeNull()
+    }
+
+    @Test
+    fun `execute by cql with named params in coroutines`() = runSuspendWithIO {
+        session.executeSuspending("SELECT * FROM user").one().shouldNotBeNull()
+
+        session.executeSuspending("DELETE FROM user WHERE id = :id", mapOf("id" to "WHITE")).wasApplied().shouldBeTrue()
+        session.executeSuspending("SELECT * FROM user").one().shouldBeNull()
+    }
+
+
+    @Test
+    fun `execute by statement in coroutines`() = runSuspendWithIO {
+        session.executeSuspending(statementOf("SELECT * FROM user")).one().shouldNotBeNull()
+
+        session.executeSuspending(statementOf("DELETE FROM user WHERE id = 'WHITE'")).wasApplied().shouldBeTrue()
+        session.executeSuspending(statementOf("SELECT * FROM user")).one().shouldBeNull()
+    }
+}
