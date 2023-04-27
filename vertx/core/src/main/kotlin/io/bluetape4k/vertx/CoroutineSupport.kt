d23d01b2180f169e6cfb9b9707a9a46e10ad1416
Debop <debop@hpcnt.com>
2023-04-27 11:27:00 +0900

    rename kotlin files

--- vertx/core/src/main/kotlin/io/bluetape4k/vertx/CoroutineSupport.kt
+++ vertx/core/src/main/kotlin/io/bluetape4k/vertx/CoroutineSupport.kt
@@ -4,6 +4,7 @@ import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Get [CoroutineDispatcher] of current [Vertx] instance.
@@ -13,7 +14,7 @@ fun currentVertxDispatcher(): CoroutineDispatcher = currentVertx().dispatcher()
 /**
  * 현 [Vertx]의 `dispatcher()`를 사용하는 [CoroutineScope]를 빌드합니다.
  */
-fun Vertx.asCoroutineScope(): CoroutineScope = CoroutineScope(this.dispatcher())
+fun Vertx.asCoroutineScope(): CoroutineScope = CoroutineScope(this.dispatcher() as CoroutineContext)
 
 /**
  * Current [Vertx]의 Thread를 사용하는 [CoroutineScope]를 빌드합니다.
