a04c9c738f7b9a8549be00c91ffc7a39b129708b
Debop <debop@hpcnt.com>
2023-05-18 23:21:02 +0900

    RedissonLeaderElection 멀티스레딩 테스트 추가

--- data/redis/src/test/kotlin/io/bluetape4k/data/redis/redisson/leader/RedissonLeaderElectionTest.kt
+++ data/redis/src/test/kotlin/io/bluetape4k/data/redis/redisson/leader/RedissonLeaderElectionTest.kt
@@ -2,13 +2,16 @@ package io.bluetape4k.data.redis.redisson.leader
 
 import io.bluetape4k.concurrent.futureOf
 import io.bluetape4k.data.redis.redisson.AbstractRedissonTest
+import io.bluetape4k.junit5.concurrency.MultithreadingTester
 import io.bluetape4k.logging.KLogging
 import io.bluetape4k.logging.debug
+import kotlinx.atomicfu.atomic
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 class RedissonLeaderElectionTest: AbstractRedissonTest() {
 
@@ -79,5 +82,77 @@ class RedissonLeaderElectionTest: AbstractRedissonTest() {
         countDownLatch.await(5, TimeUnit.SECONDS)
         future1.get() shouldBeEqualTo 42
         future2.get() shouldBeEqualTo 43
+    }
+
+    @Test
+    fun `run action if leader in multi threading`() {
+        val lockName = randomName()
+        val leaderElection = RedissonLeaderElection(redissonClient)
+
+        val task1 = atomic(0)
+        val task2 = atomic(0)
+
+        MultithreadingTester()
+            .numThreads(36)
+            .roundsPerThread(4)
+            .add {
+                leaderElection.runIfLeader(lockName) {
+                    log.debug { "작업 1 을 시작합니다. task1=${task1.value}" }
+                    task1.incrementAndGet()
+                    Thread.sleep(Random.nextLong(5, 10))
+                    log.debug { "작업 1 을 종료합니다. task1=${task1.value}" }
+                }
+            }
+            .add {
+                leaderElection.runIfLeader(lockName) {
+                    log.debug { "작업 2 을 시작합니다. task2=${task2.value}" }
+                    task2.incrementAndGet()
+                    Thread.sleep(Random.nextLong(5, 10))
+                    log.debug { "작업 2 을 종료합니다. task2=${task2.value}" }
+                }
+            }
+            .run()
+
+        task1.value shouldBeEqualTo 36 * 2
+        task2.value shouldBeEqualTo 36 * 2
+    }
+
+    @Test
+    fun `run async action if leader in multi threading`() {
+        val lockName = randomName()
+        val leaderElection = RedissonLeaderElection(redissonClient)
+
+        val task1 = atomic(0)
+        val task2 = atomic(0)
+
+        MultithreadingTester()
+            .numThreads(36)
+            .roundsPerThread(4)
+            .add {
+                leaderElection.runAsyncIfLeader(lockName) {
+                    futureOf {
+                        log.debug { "작업 1 을 시작합니다. task1=${task1.value}" }
+                        task1.incrementAndGet()
+                        Thread.sleep(Random.nextLong(5, 10))
+                        log.debug { "작업 1 을 종료합니다. task1=${task1.value}" }
+                        42
+                    }
+                }.join()
+            }
+            .add {
+                leaderElection.runAsyncIfLeader(lockName) {
+                    futureOf {
+                        log.debug { "작업 2 을 시작합니다. task2=${task2.value}" }
+                        task2.incrementAndGet()
+                        Thread.sleep(Random.nextLong(5, 10))
+                        log.debug { "작업 2 을 종료합니다. task2=${task2.value}" }
+                        43
+                    }
+                }.join()
+            }
+            .run()
+
+        task1.value shouldBeEqualTo 36 * 2
+        task2.value shouldBeEqualTo 36 * 2
     }
 }
