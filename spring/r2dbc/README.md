e50d9ecec0d32aee7b218934f524a5169be615de
Debop <sunghyouk.bae@gmail.com>
2023-05-25 23:45:43 +0900

    bluetape4k-spring-r2dbc 모듈 개발 중

---
+++ spring/r2dbc/README.md
@@ -0,0 +1,6 @@
+# Module Bluetape4k Spring R2DBC
+
+## 리소스
+
+* [Github: r2dbc-e4k](https://github.com/rjaros/r2dbc-e4k)
+* [Spring Data R2DBC and Kotlin Coroutines](https://xebia.com/blog/spring-data-r2dbc-and-kotlin-coroutines/)
---
+++ spring/r2dbc/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("plugin.spring")
+}
+
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    api(project(":bluetape4k-core"))
+    api(project(":bluetape4k-spring-support"))
+    testImplementation(project(":bluetape4k-junit5"))
+
+    // Coroutines
+    api(project(":bluetape4k-coroutines"))
+    api(Libs.kotlinx_coroutines_core)
+    api(Libs.kotlinx_coroutines_reactor)
+    testImplementation(Libs.kotlinx_coroutines_test)
+
+    api(Libs.springBootStarter("data-r2dbc"))
+    implementation("io.r2dbc:r2dbc-h2")
+
+    testImplementation(Libs.springBootStarter("test")) {
+        exclude(group = "junit", module = "junit")
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation(Libs.reactor_test)
+}
