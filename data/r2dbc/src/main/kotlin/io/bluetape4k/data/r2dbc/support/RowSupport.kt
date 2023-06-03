65e84cc8afeabc9a8ae8e8f58b3d42b03f9fe734
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2023-06-03 13:56:21 +0900

    xxxOrNull 함수 수정

--- data/r2dbc/src/main/kotlin/io/bluetape4k/data/r2dbc/support/RowSupport.kt
+++ data/r2dbc/src/main/kotlin/io/bluetape4k/data/r2dbc/support/RowSupport.kt
@@ -2,12 +2,21 @@ package io.bluetape4k.data.r2dbc.support
 
 import io.bluetape4k.support.asBigDecimal
 import io.bluetape4k.support.asBigDecimalOrNull
+import io.bluetape4k.support.asBooleanOrNull
 import io.bluetape4k.support.asByteArray
 import io.bluetape4k.support.asByteArrayOrNull
+import io.bluetape4k.support.asByteOrNull
 import io.bluetape4k.support.asChar
 import io.bluetape4k.support.asCharOrNull
 import io.bluetape4k.support.asDate
 import io.bluetape4k.support.asDateOrNull
+import io.bluetape4k.support.asDouble
+import io.bluetape4k.support.asDoubleOrNull
+import io.bluetape4k.support.asFloat
+import io.bluetape4k.support.asFloatOrNull
+import io.bluetape4k.support.asInstant
+import io.bluetape4k.support.asInstantOrNull
+import io.bluetape4k.support.asIntOrNull
 import io.bluetape4k.support.asLocalDate
 import io.bluetape4k.support.asLocalDateOrNull
 import io.bluetape4k.support.asLocalDateTime
@@ -18,10 +27,16 @@ import io.bluetape4k.support.asLong
 import io.bluetape4k.support.asLongOrNull
 import io.bluetape4k.support.asOffsetDateTime
 import io.bluetape4k.support.asOffsetDateTimeOrNull
+import io.bluetape4k.support.asShortOrNull
 import io.bluetape4k.support.asString
 import io.bluetape4k.support.asStringOrNull
+import io.bluetape4k.support.asTimestamp
+import io.bluetape4k.support.asTimestampOrNull
+import io.bluetape4k.support.asUUIDOrNull
 import io.r2dbc.spi.Row
 import java.math.BigDecimal
+import java.sql.Timestamp
+import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -35,28 +50,43 @@ inline fun <reified T: Any> Row.getAsOrNull(name: String): T? = get(name, T::cla
 
 fun Row.boolean(index: Int): Boolean = getAs<Boolean>(index)
 fun Row.boolean(name: String): Boolean = getAs<Boolean>(name)
-fun Row.booleanOrNull(index: Int): Boolean? = getAsOrNull<Boolean>(index)
-fun Row.booleanOrNull(name: String): Boolean? = getAsOrNull<Boolean>(name)
+fun Row.booleanOrNull(index: Int): Boolean? = get(index).asBooleanOrNull()
+fun Row.booleanOrNull(name: String): Boolean? = get(name).asBooleanOrNull()
 
 fun Row.char(index: Int): Char = get(index).asChar()
 fun Row.char(name: String): Char = get(name).asChar()
 fun Row.charOrNull(index: Int): Char? = get(index).asCharOrNull()
 fun Row.charOrNull(name: String): Char? = get(name).asCharOrNull()
 
+fun Row.byte(index: Int): Byte = getAs<Byte>(index)
+fun Row.byte(name: String): Byte = getAs<Byte>(name)
+fun Row.byteOrNull(index: Int): Byte? = get(index).asByteOrNull()
+fun Row.byteOrNull(name: String): Byte? = get(name).asByteOrNull()
+
 fun Row.short(index: Int): Short = getAs<Short>(index)
 fun Row.short(name: String): Short = getAs<Short>(name)
-fun Row.shortOrNull(index: Int): Short? = getAsOrNull<Short>(index)
-fun Row.shortOrNull(name: String): Short? = getAsOrNull<Short>(name)
+fun Row.shortOrNull(index: Int): Short? = get(index).asShortOrNull()
+fun Row.shortOrNull(name: String): Short? = get(name).asShortOrNull()
 
 fun Row.int(index: Int): Int = getAs<Int>(index)
 fun Row.int(name: String): Int = getAs<Int>(name)
-fun Row.intOrNull(index: Int): Int? = getAsOrNull<Int>(index)
-fun Row.intOrNull(name: String): Int? = getAsOrNull<Int>(name)
+fun Row.intOrNull(index: Int): Int? = get(index).asIntOrNull()
+fun Row.intOrNull(name: String): Int? = get(name).asIntOrNull()
 
 fun Row.long(index: Int): Long = get(index).asLong()
 fun Row.long(name: String): Long = get(name).asLong()
 fun Row.longOrNull(index: Int): Long? = get(index).asLongOrNull()
 fun Row.longOrNull(name: String): Long? = get(name).asLongOrNull()
+
+fun Row.float(index: Int): Float = get(index).asFloat()
+fun Row.float(name: String): Float = get(name).asFloat()
+fun Row.floatOrNull(index: Int): Float? = get(index).asFloatOrNull()
+fun Row.floatOrNull(name: String): Float? = get(name).asFloatOrNull()
+
+fun Row.double(index: Int): Double = get(index).asDouble()
+fun Row.double(name: String): Double = get(name).asDouble()
+fun Row.doubleOrNull(index: Int): Double? = get(index).asDoubleOrNull()
+fun Row.doubleOrNull(name: String): Double? = get(name).asDoubleOrNull()
 
 fun Row.bigDecimal(index: Int): BigDecimal = get(index).asBigDecimal()
 fun Row.bigDecimal(name: String): BigDecimal = get(name).asBigDecimal()
@@ -77,6 +107,16 @@ fun Row.date(index: Int): Date = get(index).asDate()
 fun Row.date(name: String): Date = get(name).asDate()
 fun Row.dateOrNull(index: Int): Date? = get(index).asDateOrNull()
 fun Row.dateOrNull(name: String): Date? = get(name).asDateOrNull()
+
+fun Row.timestamp(index: Int): Timestamp = get(index).asTimestamp()
+fun Row.timestamp(name: String): Timestamp = get(name).asTimestamp()
+fun Row.timestampOrNull(index: Int): Timestamp? = get(index).asTimestampOrNull()
+fun Row.timestampOrNull(name: String): Timestamp? = get(name).asTimestampOrNull()
+
+fun Row.instant(index: Int): Instant = get(index).asInstant()
+fun Row.instant(name: String): Instant = get(name).asInstant()
+fun Row.instantOrNull(index: Int): Instant? = get(index).asInstantOrNull()
+fun Row.instantOrNull(name: String): Instant? = get(name).asInstantOrNull()
 
 fun Row.localDate(index: Int): LocalDate = get(index).asLocalDate()
 fun Row.localDate(name: String): LocalDate = get(name).asLocalDate()
@@ -100,5 +140,5 @@ fun Row.offsetDateTimeOrNull(name: String): OffsetDateTime? = get(name).asOffset
 
 fun Row.uuid(index: Int): UUID = getAs<UUID>(index)
 fun Row.uuid(name: String): UUID = getAs<UUID>(name)
-fun Row.uuidOrNull(index: Int): UUID? = getAsOrNull<UUID>(index)
-fun Row.uuidOrNull(name: String): UUID? = getAsOrNull<UUID>(name)
+fun Row.uuidOrNull(index: Int): UUID? = get(index).asUUIDOrNull()
+fun Row.uuidOrNull(name: String): UUID? = get(name).asUUIDOrNull()
