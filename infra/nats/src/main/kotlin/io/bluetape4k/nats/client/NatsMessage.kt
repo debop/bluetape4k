796b8f890cddcf2422907b203d50c1751aae11ef
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2023-07-13 20:48:19 +0900

    update natsMessageOf

--- infra/nats/src/main/kotlin/io/bluetape4k/nats/client/NatsMessage.kt
+++ infra/nats/src/main/kotlin/io/bluetape4k/nats/client/NatsMessage.kt
@@ -1,6 +1,5 @@
 package io.bluetape4k.nats.client
 
-import io.bluetape4k.support.toUtf8Bytes
 import io.nats.client.Message
 import io.nats.client.impl.Headers
 import io.nats.client.impl.NatsMessage
@@ -13,14 +12,24 @@ fun natsMessageOf(message: Message) = NatsMessage(message)
 
 fun natsMessageOf(
     subject: String,
-    replyTo: String,
-    headers: Headers? = null,
     data: ByteArray,
-): NatsMessage = NatsMessage(subject, replyTo, headers, data)
+    replyTo: String? = null,
+    headers: Headers? = null,
+): NatsMessage = natsMessage {
+    subject(subject)
+    data(data)
+    replyTo?.run { replyTo(this) }
+    headers?.run { headers(this) }
+}
 
 fun natsMessageOf(
     subject: String,
-    replyTo: String,
-    headers: Headers? = null,
     data: String,
-): NatsMessage = NatsMessage(subject, replyTo, headers, data.toUtf8Bytes())
+    replyTo: String? = null,
+    headers: Headers? = null,
+): NatsMessage = natsMessage {
+    subject(subject)
+    data(data)
+    replyTo?.run { replyTo(this) }
+    headers?.run { headers(this) }
+}
