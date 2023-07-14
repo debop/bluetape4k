df8ded504b7737df4293094dec85144a48b19c33
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2023-07-14 21:06:47 +0900

    add javvers-core module

--- javers/spring/build.gradle.kts
+++ javers/spring/build.gradle.kts
@@ -18,6 +18,10 @@ dependencies {
     testImplementation(project(":bluetape4k-junit5"))
     testImplementation(project(":bluetape4k-testcontainers"))
 
+    // Javers
+    api(project(":bluetape4k-javers-core"))
+    // bluetape4k-javers-core 의 테스트 코드를 재활용하기 위해 참조합니다.
+    testImplementation(project(path = ":bluetape4k-javers-core", configuration = "testJar"))
     api(Libs.javers_core)
     api(Libs.javers_spring)
     testImplementation(Libs.guava)
