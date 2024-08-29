e06462f544f8fd37d8081df55c047490db59b61b
debop <debop.bae@healingpaper.com>
2024-08-29 23:14:48 +0900

    Polishing
    Signed-off-by: debop <debop.bae@healingpaper.com>

--- bluetape4k/testcontainers/src/test/kotlin/io/bluetape4k/testcontainers/jdbc/PostgreSQLServerTest.kt
+++ bluetape4k/testcontainers/src/test/kotlin/io/bluetape4k/testcontainers/jdbc/PostgreSQLServerTest.kt
@@ -12,7 +12,6 @@ class PostgreSQLServerTest: AbstractJdbcServerTest() {
     fun `launch PostgreSQL server`() {
         PostgreSQLServer().use { postgres ->
             postgres.start()
-            Thread.sleep(10)
             assertConnection(postgres)
         }
     }
@@ -21,7 +20,6 @@ class PostgreSQLServerTest: AbstractJdbcServerTest() {
     fun `launch PostgreSQL server with default port`() {
         PostgreSQLServer(useDefaultPort = true).use { postgres ->
             postgres.start()
-            Thread.sleep(100)
             postgres.port shouldBeEqualTo PostgreSQLServer.PORT
             assertConnection(postgres)
         }
